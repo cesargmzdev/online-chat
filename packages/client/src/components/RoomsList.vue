db56eee97fb7d693adb9f104df080d3be08fd7bd
César Gómez <cesargomez015@gmail.com>
2024-05-31 11:40:17 +0200

    chore: Update RoomsList component with scrollable list and animation

--- packages/client/src/components/RoomsList.vue
+++ packages/client/src/components/RoomsList.vue
@@ -45,27 +45,76 @@ const leaveAllAddedRooms = async () => {
   socket.on('roomCount', (rooms) => {
     arrayRooms.value = [...new Set([...rooms])];
   });
+  alert('All added rooms cleared');
+};
+
+const newRoomAdded = () => {
+  const roomsListUl = document.querySelector('#roomsListUl');
+  roomsListUl.scrollTop = roomsListUl.scrollHeight;
 };
 </script>
 
 <template>
-  <div class="w-full h-full grid place-items-center">
-    <div class="flex w-full px-5 items-center justify-between">
+  <div class="grid h-full w-full place-items-center">
+    <div class="flex w-full items-center justify-between px-5">
       <h2 class="text-2xl text-[var(--myGreenColor)]">Rooms</h2>
-      <button type="button" class="border-2 rounded-md p-1 m-2" @click="leaveAllAddedRooms">
+      <button type="button" class="m-2 rounded-md border-2 p-1" @click="leaveAllAddedRooms">
         Leave all added rooms
       </button>
-      <button type="button" class="border-2 rounded-md p-1 m-2" @click="toggleJoinRoomForm">
+      <button type="button" class="m-2 rounded-md border-2 p-1" @click="toggleJoinRoomForm">
         Join Room
       </button>
     </div>
-    <ul>
-      <li v-for="room in reversedArrayRooms" :key="room">
-        <button type="button" @click="setRoom(room)" class="border-2 rounded-md p-1 m-2">
+    <transition-group
+      name="list"
+      id="roomsListUl"
+      tag="ul"
+      class="scrollbar max-h-[70%] w-full overflow-y-auto overflow-x-hidden"
+      @change="newRoomAdded"
+    >
+      <li
+        v-for="(room, index) in reversedArrayRooms"
+        :key="room"
+        class="list-item flex-col gap-4 py-4"
+        :style="{ 'animation-delay': `${index * 0.15}s` }"
+      >
+        <button type="button" @click="setRoom(room)" class="text-lg">
           {{ room }}
         </button>
+        <hr
+          v-if="
+            reversedArrayRooms.length > 1 &&
+            room !== reversedArrayRooms[reversedArrayRooms.length - 1]
+          "
+          class="mt-3 flex w-1/3 self-center"
+        />
       </li>
-    </ul>
+    </transition-group>
     <JoinRoomDialog :show="showJoinRoomForm" :toggle="toggleJoinRoomForm" />
   </div>
 </template>
+
+<style scoped>
+.scrollbar {
+  scrollbar-width: thin;
+  scrollbar-color: var(--myBlackColor) var(--myGreenColor);
+}
+.list-item {
+  position: relative;
+  display: flex;
+  animation: fadeIn 0.5s linear both;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    top: 100px;
+  }
+  75% {
+    opacity: 0.5;
+    top: 0px;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
